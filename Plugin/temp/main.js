9c60b6c179f5cdec113954482d7f90c8dff5d754
LiuYangArt <liuyangart@gmail.com>
2025-12-02 14:07:04 +0800

    update

--- Plugin/temp/main.js
+++
@@ -1,9 +0,0 @@
-const { core } = require('photoshop');
-
-document.getElementById("btnTest").addEventListener("click", async () => {
-    try {
-        await core.showAlert({ message: "PS Banana: Test Successful!" });
-    } catch (e) {
-        console.error(e);
-    }
-});
